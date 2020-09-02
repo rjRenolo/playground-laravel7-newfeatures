0b55e517672790831c8f8aa0452dca7438aa3d70
rjRenolo <rjrenolo14@gmail.com>
2020-09-02 09:14:02 +0800

    Laravel String functions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+///////////
+// after //
+///////////
+Route::get('/after', function(){
+    ///////////////////////////////////////
+    // After Parameter is case-sensitive //
+    ///////////////////////////////////////
+    $sliced = Str::of('Something to be slice-off')->after('Something to be ');
+    dd($sliced);
+});
+
+///////////////
+// afterLast //
+///////////////
+Route::get('/afterlast', function(){
+    $sliced = Str::of('Illuminate\Support\Str')->afterLast('\\'); // Escaping character with backslach
+    dd($sliced);
+});
+
+//////////////////////////////////////////////////
+// explode                                      //
+// kinda important thing to remember            //
+// making an array from string with a separator //
+//////////////////////////////////////////////////
+Route::get('/explode', function(){
+    $exploded = Str::of('Something to separate')->explode(' ');
+    dd($exploded);
+});
+
+//////////////////////////////////////
+// limit                            //
+// fun function for making excerpts //
+//////////////////////////////////////
+Route::get('/limit', function(){
+    $limited = Str::of('The quick brown fox jumps over the lazy dog')->limit(10, '...');
+    dd($limited);
+});
+
+//////////////////////////////////////////////////
+// and many more kinda useless string functions //
+//////////////////////////////////////////////////
